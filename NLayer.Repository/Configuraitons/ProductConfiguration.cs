4c7251fae9b4e110529b30c614fb5732f1f9e210
Mikdat Can Şimşek <115544143+mikdatcansimsek@users.noreply.github.com>
2024-05-12 18:02:17 +0300

    12.05.2024

---
+++ NLayer.Repository/Configuraitons/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configuraitons
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Stock).IsRequired();
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
+            builder.ToTable("Products");
+
+            builder.HasOne(x => x.Category).WithMany(x => x.Products).HasForeignKey(x => x.CategoryId);
+        }
+    }
+}
